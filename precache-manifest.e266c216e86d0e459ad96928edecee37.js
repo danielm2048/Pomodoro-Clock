f5f7b49673c1fcc32cd8beecf0b8e5ba2ad9e698
danielm2048 <danielmimoun98@gmail.com>
2019-12-28 13:18:27 +0200

    Updates

---
+++ precache-manifest.e266c216e86d0e459ad96928edecee37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb51e874d5dd232fb85130c8ee1920ab",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "76dc103cc46a9cd4aea5",
+    "url": "/Pomodoro-Clock/static/css/main.e9beb2b4.chunk.css"
+  },
+  {
+    "revision": "594dfc0869e917a93d4e",
+    "url": "/Pomodoro-Clock/static/js/2.7c8ed970.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pomodoro-Clock/static/js/2.7c8ed970.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76dc103cc46a9cd4aea5",
+    "url": "/Pomodoro-Clock/static/js/main.e905e961.chunk.js"
+  },
+  {
+    "revision": "4205def8a1939bf281b0",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.fdf2b879.js"
+  },
+  {
+    "revision": "9bdca1db4f1daaba8c4a7a370bcaa689",
+    "url": "/Pomodoro-Clock/static/media/beep.9bdca1db.mp3"
+  }
+]);
